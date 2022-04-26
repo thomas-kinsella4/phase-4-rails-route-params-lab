8b7755dde4303b68bf898419049c6881a7d5a7a2
Thomas Kinsella <thomasjkinsella4@gmail.com>
2022-04-26 16:15:32 -0400

    commit tree a484d54b45975c34d035d87923c93015ae64a184

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,18 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    # students = Student.all
+    # render json: students
+    students = if params[:name] 
+      Student.where(name: params[:name])
+    else 
+      Student.all
+    end
     render json: students
+  end
+
+  def show
+    render json: Student.all.find(params[:id])
   end
 
 end
